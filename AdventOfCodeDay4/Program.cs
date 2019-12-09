f5cac99c227fe1e9f2297002cbd700cf2f2b7040
Cameron Asbury <CameronAsbury@openarc.net>
2019-12-09 12:43:26 -0500

    Finished part 1 of day 4

--- AdventOfCodeDay4/Program.cs
+++ AdventOfCodeDay4/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AdventOfCodeDay4
 {
@@ -6,6 +7,50 @@ namespace AdventOfCodeDay4
     {
         static void Main(string[] args)
         {
+            int lowerLimit = 256310;
+            int upperLimit = 732736;
+
+            List<string> rawListOfPasswords = new List<string>();
+
+            for (int i = lowerLimit ; i <= upperLimit; i++)
+            {
+                string s = i.ToString();
+                int digitsIncreaseCounter = 0;
+                bool isIncreasing;
+                bool isAdjacent = false;
+
+                for (int j = 0; j < s.Length - 1; j++)
+                {
+                    int test = Convert.ToInt32(s.Substring(j));
+                    if (Convert.ToInt32(s[j]) <= Convert.ToInt32(s[j+1]))
+                    {
+                        digitsIncreaseCounter++;
+                    }
+                    for (int k = 0; k < s.Length -1; k++)
+                    {
+                        if (s[k] == s[k + 1])
+                        {
+                            isAdjacent = true;
+                        }
+                    }
+                    if (digitsIncreaseCounter == s.Length - 1 && isAdjacent)
+                    {
+                        rawListOfPasswords.Add(s);
+                    }
+                }
+            }
+
+            foreach (string s in rawListOfPasswords)
+            {
+
+            }
+
+            foreach(string s in rawListOfPasswords)
+            {
+                Console.WriteLine(s);
+            }
+            Console.WriteLine("Number of elements: " + rawListOfPasswords.Count);
+
         }
     }
 }
